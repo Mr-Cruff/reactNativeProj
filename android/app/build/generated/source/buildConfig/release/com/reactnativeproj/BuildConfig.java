ca73b6ff62c03c2e9270a34a1212f87e51e1d5fd
ckwilliams <ckwilliams@jabgl.com>
2023-12-14 19:31:56 -0500

    build changes

--- android/app/build/generated/source/buildConfig/release/com/reactnativeproj/BuildConfig.java
+++ android/app/build/generated/source/buildConfig/release/com/reactnativeproj/BuildConfig.java
@@ -7,8 +7,8 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "com.reactnativeproj";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 21;
-  public static final String VERSION_NAME = "1.0.20 - UAT";
+  public static final int VERSION_CODE = 22;
+  public static final String VERSION_NAME = "1.0.21 - UAT";
   // Field from default config.
   public static final boolean IS_NEW_ARCHITECTURE_ENABLED = false;
 }
